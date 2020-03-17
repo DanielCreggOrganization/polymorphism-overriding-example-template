8cd9f0016b9a35af0a9949ff7f1f5ebd73ab7d92
Daniel Cregg <daniel.cregg@gmit.ie>
2020-03-17 16:36:23 +0000

    Adding getters and setters

--- TwoDShape.java
+++ TwoDShape.java
@@ -1,11 +1,29 @@
 public class TwoDShape {
-  
-  // Instance Varibales
-  private double width;
-  private double height;
-  
-  public double findArea(){
-    return 0.0;
-  } 
+
+    // Instance Varibales
+    private double width;
+    private double height;
+
+    // Getters and Setters
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    // Area method
+    public double findArea() {
+        return 0.0d;
+    }
 
 }
